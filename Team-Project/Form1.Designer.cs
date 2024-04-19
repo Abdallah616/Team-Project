e0cdb0e2555b0a8e06dd5838ac9d437718d9b197
Abdallah616 <yassinabdou59@gmail.com>
2024-04-19 14:23:15 +0200

    hi

--- Team-Project/Form1.Designer.cs
+++ Team-Project/Form1.Designer.cs
@@ -28,10 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            panel2 = new Panel();
-            listBox3 = new ListBox();
-            listBox2 = new ListBox();
-            listBox1 = new ListBox();
             panel3 = new Panel();
             NameButton = new Button();
             tx_name = new TextBox();
@@ -45,53 +41,23 @@
             RateButton = new Button();
             tx_rate = new TextBox();
             label3 = new Label();
-            textBox4 = new TextBox();
+            tx_days = new TextBox();
             label4 = new Label();
-            panel2.SuspendLayout();
+            OpenFile_bt = new Button();
+            ReadFromFile_bt = new Button();
+            InsertToFile_bt = new Button();
+            Clear_bt = new Button();
+            StartOfFile_bt = new Button();
+            EndOfFile_bt = new Button();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            panel2 = new Panel();
             panel3.SuspendLayout();
             panel1.SuspendLayout();
             panel4.SuspendLayout();
+            panel2.SuspendLayout();
             SuspendLayout();
-            // 
-            // panel2
-            // 
-            panel2.BackColor = Color.Transparent;
-            panel2.Controls.Add(listBox3);
-            panel2.Controls.Add(listBox2);
-            panel2.Controls.Add(listBox1);
-            panel2.Location = new Point(686, 11);
-            panel2.Name = "panel2";
-            panel2.Size = new Size(225, 542);
-            // 
-            // listBox3
-            // 
-            listBox3.BackColor = Color.White;
-            listBox3.ForeColor = SystemColors.ControlText;
-            listBox3.FormattingEnabled = true;
-            listBox3.ItemHeight = 15;
-            listBox3.Location = new Point(0, 421);
-            listBox3.Name = "listBox3";
-            listBox3.Size = new Size(201, 49);
-            // 
-            // listBox2
-            // 
-            listBox2.BackColor = Color.White;
-            listBox2.ForeColor = SystemColors.ControlText;
-            listBox2.FormattingEnabled = true;
-            listBox2.ItemHeight = 15;
-            listBox2.Location = new Point(3, 250);
-            listBox2.Name = "listBox2";
-            listBox2.Size = new Size(201, 49);
-            // 
-            // listBox1
-            // 
-            listBox1.BackColor = Color.White;
-            listBox1.ForeColor = SystemColors.InfoText;
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
-            listBox1.Location = new Point(0, 43);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(201, 49);
             // 
             // panel3
             // 
@@ -104,6 +70,7 @@
             panel3.Location = new Point(22, 11);
             panel3.Name = "panel3";
             panel3.Size = new Size(625, 131);
+            panel3.TabIndex = 6;
             // 
             // NameButton
             // 
@@ -120,7 +87,7 @@
             NameButton.TabIndex = 3;
             NameButton.Text = "INPUT DATA";
             NameButton.UseVisualStyleBackColor = false;
-            NameButton.Click += new EventHandler(NameButton_Click);
+            NameButton.Click += NameButton_Click;
             // 
             // tx_name
             // 
@@ -130,6 +97,7 @@
             tx_name.Multiline = true;
             tx_name.Name = "tx_name";
             tx_name.Size = new Size(281, 39);
+            tx_name.TabIndex = 4;
             // 
             // label2
             // 
@@ -141,6 +109,7 @@
             label2.Location = new Point(17, 50);
             label2.Name = "label2";
             label2.Size = new Size(113, 37);
+            label2.TabIndex = 5;
             label2.Text = "Name";
             // 
             // SHEET
@@ -148,13 +117,13 @@
             SHEET.BackColor = Color.Indigo;
             SHEET.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
             SHEET.ForeColor = Color.White;
-            SHEET.Location = new Point(359, 622);
+            SHEET.Location = new Point(743, 645);
             SHEET.Name = "SHEET";
-            SHEET.Size = new Size(210, 61);
+            SHEET.Size = new Size(144, 61);
             SHEET.TabIndex = 2;
             SHEET.Text = "GO TO THE SHEET";
             SHEET.UseVisualStyleBackColor = false;
-            SHEET.Click += new EventHandler(SHEET_Click);
+            SHEET.Click += SHEET_Click;
             // 
             // panel1
             // 
@@ -167,6 +136,7 @@
             panel1.Location = new Point(22, 218);
             panel1.Name = "panel1";
             panel1.Size = new Size(625, 131);
+            panel1.TabIndex = 5;
             // 
             // No_Of_HoursButton
             // 
@@ -180,9 +150,10 @@
             No_Of_HoursButton.Location = new Point(471, 52);
             No_Of_HoursButton.Name = "No_Of_HoursButton";
             No_Of_HoursButton.Size = new Size(125, 40);
+            No_Of_HoursButton.TabIndex = 0;
             No_Of_HoursButton.Text = "INPUT DATA";
             No_Of_HoursButton.UseVisualStyleBackColor = false;
-            No_Of_HoursButton.Click += new EventHandler(Hourbutton_Click);
+            No_Of_HoursButton.Click += Hourbutton_Click;
             // 
             // tx_hour
             // 
@@ -191,6 +162,7 @@
             tx_hour.Multiline = true;
             tx_hour.Name = "tx_hour";
             tx_hour.Size = new Size(281, 39);
+            tx_hour.TabIndex = 1;
             // 
             // label1
             // 
@@ -202,6 +174,7 @@
             label1.Location = new Point(17, 50);
             label1.Name = "label1";
             label1.Size = new Size(118, 37);
+            label1.TabIndex = 2;
             label1.Text = "Hours";
             // 
             // panel4
@@ -215,6 +188,7 @@
             panel4.Location = new Point(22, 389);
             panel4.Name = "panel4";
             panel4.Size = new Size(625, 131);
+            panel4.TabIndex = 4;
             // 
             // RateButton
             // 
@@ -228,9 +202,10 @@
             RateButton.Location = new Point(471, 52);
             RateButton.Name = "RateButton";
             RateButton.Size = new Size(125, 40);
+            RateButton.TabIndex = 0;
             RateButton.Text = "INPUT DATA";
             RateButton.UseVisualStyleBackColor = false;
-            RateButton.Click +=new EventHandler(RateButton_Click);
+            RateButton.Click += RateButton_Click;
             // 
             // tx_rate
             // 
@@ -239,6 +214,7 @@
             tx_rate.Multiline = true;
             tx_rate.Name = "tx_rate";
             tx_rate.Size = new Size(281, 39);
+            tx_rate.TabIndex = 1;
             // 
             // label3
             // 
@@ -250,14 +226,16 @@
             label3.Location = new Point(17, 50);
             label3.Name = "label3";
             label3.Size = new Size(92, 37);
+            label3.TabIndex = 2;
             label3.Text = "Rate";
             // 
-            // textBox4
+            // tx_days
             // 
-            textBox4.Font = new Font("Palatino Linotype", 18F, FontStyle.Italic, GraphicsUnit.Point);
-            textBox4.Location = new Point(376, 554);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(242, 40);
+            tx_days.Font = new Font("Palatino Linotype", 18F, FontStyle.Italic, GraphicsUnit.Point);
+            tx_days.Location = new Point(376, 554);
+            tx_days.Name = "tx_days";
+            tx_days.Size = new Size(242, 40);
+            tx_days.TabIndex = 1;
             // 
             // label4
             // 
@@ -269,16 +247,150 @@
             label4.Location = new Point(186, 556);
             label4.Name = "label4";
             label4.Size = new Size(160, 27);
+            label4.TabIndex = 0;
             label4.Text = "Days Works";
+            // 
+            // OpenFile_bt
+            // 
+            OpenFile_bt.BackColor = Color.Indigo;
+            OpenFile_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            OpenFile_bt.ForeColor = Color.White;
+            OpenFile_bt.Location = new Point(39, 645);
+            OpenFile_bt.Name = "OpenFile_bt";
+            OpenFile_bt.Size = new Size(92, 61);
+            OpenFile_bt.TabIndex = 7;
+            OpenFile_bt.Text = "Open File";
+            OpenFile_bt.UseVisualStyleBackColor = false;
+            OpenFile_bt.Click += OpenFile_bt_Click;
+            // 
+            // ReadFromFile_bt
+            // 
+            ReadFromFile_bt.BackColor = Color.Indigo;
+            ReadFromFile_bt.Enabled = false;
+            ReadFromFile_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            ReadFromFile_bt.ForeColor = Color.White;
+            ReadFromFile_bt.Location = new Point(137, 645);
+            ReadFromFile_bt.Name = "ReadFromFile_bt";
+            ReadFromFile_bt.Size = new Size(115, 61);
+            ReadFromFile_bt.TabIndex = 8;
+            ReadFromFile_bt.Text = "Read From File";
+            ReadFromFile_bt.UseVisualStyleBackColor = false;
+            ReadFromFile_bt.Click += ReadFromFile_bt_Click;
+            // 
+            // InsertToFile_bt
+            // 
+            InsertToFile_bt.BackColor = Color.Indigo;
+            InsertToFile_bt.Enabled = false;
+            InsertToFile_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            InsertToFile_bt.ForeColor = Color.White;
+            InsertToFile_bt.Location = new Point(269, 645);
+            InsertToFile_bt.Name = "InsertToFile_bt";
+            InsertToFile_bt.Size = new Size(111, 61);
+            InsertToFile_bt.TabIndex = 9;
+            InsertToFile_bt.Text = "Insert To File";
+            InsertToFile_bt.UseVisualStyleBackColor = false;
+            InsertToFile_bt.Click += InsertToFile_bt_Click;
+            // 
+            // Clear_bt
+            // 
+            Clear_bt.BackColor = Color.Indigo;
+            Clear_bt.Enabled = false;
+            Clear_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            Clear_bt.ForeColor = Color.White;
+            Clear_bt.Location = new Point(401, 645);
+            Clear_bt.Name = "Clear_bt";
+            Clear_bt.Size = new Size(92, 61);
+            Clear_bt.TabIndex = 10;
+            Clear_bt.Text = "Clear";
+            Clear_bt.UseVisualStyleBackColor = false;
+            Clear_bt.Click += Clear_bt_Click;
+            // 
+            // StartOfFile_bt
+            // 
+            StartOfFile_bt.BackColor = Color.Indigo;
+            StartOfFile_bt.Enabled = false;
+            StartOfFile_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            StartOfFile_bt.ForeColor = Color.White;
+            StartOfFile_bt.Location = new Point(499, 645);
+            StartOfFile_bt.Name = "StartOfFile_bt";
+            StartOfFile_bt.Size = new Size(92, 61);
+            StartOfFile_bt.TabIndex = 11;
+            StartOfFile_bt.Text = "Start Of File";
+            StartOfFile_bt.UseVisualStyleBackColor = false;
+            StartOfFile_bt.Click += StartOfFile_bt_Click;
+            // 
+            // EndOfFile_bt
+            // 
+            EndOfFile_bt.BackColor = Color.Indigo;
+            EndOfFile_bt.Enabled = false;
+            EndOfFile_bt.Font = new Font("Britannic Bold", 10.2F, FontStyle.Regular, GraphicsUnit.Point);
+            EndOfFile_bt.ForeColor = Color.White;
+            EndOfFile_bt.Location = new Point(597, 645);
+            EndOfFile_bt.Name = "EndOfFile_bt";
+            EndOfFile_bt.Size = new Size(92, 61);
+            EndOfFile_bt.TabIndex = 12;
+            EndOfFile_bt.Text = "End Of File";
+            EndOfFile_bt.UseVisualStyleBackColor = false;
+            EndOfFile_bt.Click += EndOfFile_bt_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.BackColor = Color.White;
+            listBox1.ForeColor = SystemColors.InfoText;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(0, 43);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(201, 49);
+            listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            listBox2.BackColor = Color.White;
+            listBox2.ForeColor = SystemColors.ControlText;
+            listBox2.FormattingEnabled = true;
+            listBox2.ItemHeight = 15;
+            listBox2.Location = new Point(3, 250);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(201, 49);
+            listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            listBox3.BackColor = Color.White;
+            listBox3.ForeColor = SystemColors.ControlText;
+            listBox3.FormattingEnabled = true;
+            listBox3.ItemHeight = 15;
+            listBox3.Location = new Point(0, 421);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(201, 49);
+            listBox3.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.Controls.Add(listBox3);
+            panel2.Controls.Add(listBox2);
+            panel2.Controls.Add(listBox1);
+            panel2.Location = new Point(1006, 36);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(225, 484);
+            panel2.TabIndex = 3;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.WhatsApp_Image_2023_12_08_at_21_12_35_0ff9bb80;
-            ClientSize = new Size(913, 727);
+            ClientSize = new Size(1262, 727);
+            Controls.Add(EndOfFile_bt);
+            Controls.Add(StartOfFile_bt);
+            Controls.Add(Clear_bt);
+            Controls.Add(InsertToFile_bt);
+            Controls.Add(ReadFromFile_bt);
+            Controls.Add(OpenFile_bt);
             Controls.Add(label4);
-            Controls.Add(textBox4);
+            Controls.Add(tx_days);
             Controls.Add(SHEET);
             Controls.Add(panel2);
             Controls.Add(panel4);
@@ -289,22 +401,18 @@
             Name = "Form1";
             Text = "SALARY ";
             Load += Form1_Load;
-            panel2.ResumeLayout(false);
             panel3.ResumeLayout(false);
             panel3.PerformLayout();
             panel1.ResumeLayout(false);
             panel1.PerformLayout();
             panel4.ResumeLayout(false);
             panel4.PerformLayout();
+            panel2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ListBox listBox3;
-        private System.Windows.Forms.ListBox listBox2;
         private System.Windows.Forms.Panel panel3;
         private System.Windows.Forms.Button NameButton;
         private System.Windows.Forms.TextBox tx_name;
@@ -318,7 +426,17 @@
         private System.Windows.Forms.Button RateButton;
         private System.Windows.Forms.TextBox tx_rate;
         private System.Windows.Forms.Label label3;
-        private TextBox textBox4;
+        private TextBox tx_days;
         private Label label4;
+        private Button OpenFile_bt;
+        private Button ReadFromFile_bt;
+        private Button InsertToFile_bt;
+        private Button Clear_bt;
+        private Button StartOfFile_bt;
+        private Button EndOfFile_bt;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private Panel panel2;
     }
 }
--- Team-Project/Form1.cs
+++ Team-Project/Form1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Office.Interop.Excel;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -5,9 +6,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Net.Mail;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using static System.Windows.Forms.DataFormats;
 
 namespace Team_Project
@@ -18,13 +21,14 @@ namespace Team_Project
         {
             InitializeComponent();
         }
-
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-
+        FileStream MyFile;
+        StreamReader sr;
+        StreamWriter sw;
+        string FileName;
         private void NameButton_Click(object sender, EventArgs e)
         {
             // Get the text from the multi-line text box
@@ -82,13 +86,69 @@ namespace Team_Project
         public void SHEET_Click(object sender, EventArgs e)
         {
             this.Hide();
-            Form2 form1 = new Form2(listBox1, listBox2, listBox3, textBox4);
+            Form2 form1 = new Form2(listBox1, listBox2, listBox3, tx_days);
             form1.ShowDialog();
             this.Show();
         }
 
+        private void ReadFromFile_bt_Click(object sender, EventArgs e)
+        {
+            string record = sr.ReadLine();
+            string[] fields;
+            if (record != null)
+            {
+                fields = record.Split(',');
+                tx_name.Text = fields[0];
+                tx_hour.Text = fields[1];
+                tx_rate.Text = fields[2];
+                tx_days.Text = fields[3];
+            }
+            else
+            {
+                MessageBox.Show("no more records");
+                Clear_bt_Click(sender, e);
+            }
+        }
 
-      
+        private void OpenFile_bt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.Cancel)
+                return;
+            FileName = ofd.FileName;
+            MyFile = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            sw = new StreamWriter(MyFile);
+            sr = new StreamReader(MyFile);
+            MessageBox.Show("File is opened");
+            ReadFromFile_bt.Enabled = true;
+            InsertToFile_bt.Enabled = true;
+            Clear_bt.Enabled = true;
+        }
 
+        private void InsertToFile_bt_Click(object sender, EventArgs e)
+        {
+            string record = tx_name.Text + "," + tx_hour.Text + "," + tx_rate.Text + "," + tx_days.Text;
+            sw.WriteLine(record);
+            sw.Flush();
+            MessageBox.Show("Record Saved");
+        }
+
+        private void Clear_bt_Click(object sender, EventArgs e)
+        {
+            tx_days.Text = tx_hour.Text = tx_rate.Text = tx_days.Text = listBox1.Text = listBox2.Text = listBox3.Text = null;
+        }
+
+        private void StartOfFile_bt_Click(object sender, EventArgs e)
+        {
+            MyFile.Seek(0, SeekOrigin.Begin);
+            MessageBox.Show("Begin of file");
+        }
+
+        private void EndOfFile_bt_Click(object sender, EventArgs e)
+        {
+            MyFile.Seek(0, SeekOrigin.End);
+            MessageBox.Show("End of file");
+        }
     }
 }
